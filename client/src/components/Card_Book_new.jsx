c2a1f3d2d26585955e30f5ef312597d90a882d94
jeagerism <oo4dxuser@gmail.com>
2024-01-16 12:34:20 +0700

    carousel

--- client/src/components/Card_Book_new.jsx
+++ client/src/components/Card_Book_new.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import { Carousel } from 'flowbite-react';
 function Card_Book_new() {
   return (
-    <div className=' bg-blue-600 w-full h-1/2'>Card_Book_new</div>
-  )
+    <div className=" bg-blue-600 w-full h-1/2 p-2">
+      <div className="w-full h-full">
+        <Carousel>
+        <img src="https://pbs.twimg.com/media/CX7FbcJUMAABI53?format=jpg&name=4096x4096 " alt="" />
+        <img src="https://thesiamsociety.org/wp-content/uploads/2019/09/7230640.jpg " alt="" />
+        <img src="https://www.letsphuket.com/wp-content/uploads/phuket1.jpg" alt="" />
+        </Carousel>
+        
+      </div>
+    </div>
+  );
 }
 
-export default Card_Book_new
+export default Card_Book_new;
